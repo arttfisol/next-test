d6d150e899ea28cbe13fb0ac855dfac1bb9666e2
Artt <fisol3640.art@gmail.com>
2021-09-29 09:35:32 +0700

    add menu home

--- components/menu.js
+++ components/menu.js
@@ -1,5 +1,6 @@
 import { Grid, List, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material'
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts'
+import HomeIcon from '@mui/icons-material/Home'
 import InboxIcon from '@mui/icons-material/Inbox'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd'
@@ -8,6 +9,14 @@ export default function Menu ({ number, type, price }) {
   return (
     <Grid item xs={2} style={{ width: '100%', height: '100%' }}>
       <List component='nav' aria-label='main mailbox folders'>
+        <Divider />
+        <ListItemButton component='a' href='/home'>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary='Home' />
+        </ListItemButton>
+        <Divider />
         <Divider />
         <ListItemButton component='a' href='/admin/room'>
           <ListItemIcon>
